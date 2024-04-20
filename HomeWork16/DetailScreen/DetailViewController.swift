5d65e665f595c45ea79d1be88bfddbfb72130d64
Oleksandr-Matsenko <olexandr282@gmail.com>
2024-04-20 20:11:52 +0300

    Add "Back" to detailVC

--- HomeWork16/DetailScreen/DetailViewController.swift
+++ HomeWork16/DetailScreen/DetailViewController.swift
@@ -13,6 +13,7 @@ final class DetailViewController: UIViewController {
     
     var titleText: String?
     private lazy var detailLabel = UIBuilder.createLable(font: .boldSystemFont(ofSize: 40), textColor: .black)
+    private lazy var doneButton = UIBuilder.createButton(title: "Done", titleColor: .white, titleFont: .boldSystemFont(ofSize: 30), backgroundColor: .black, action: #selector(backToCell), isHiden: false, alpha: 0.0)
 
     // MARK: - Life Cycle
     
@@ -20,6 +21,7 @@ final class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         setupUI()
+        configureButton()
 
 
     }
@@ -28,15 +30,32 @@ final class DetailViewController: UIViewController {
     
     private func setupUI() {
         view.addSubview(detailLabel)
+        view.addSubview(doneButton)
+        title = "Details"
         view.backgroundColor = .green
         // Constraints
         NSLayoutConstraint.activate([
-            detailLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             detailLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             detailLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             detailLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            doneButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -40),
+            doneButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            doneButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7)
         ])
         detailLabel.text = titleText
     }
-   
+    @objc private func backToCell() {
+        dismiss(animated: true)
+    }
+    private func configureButton() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
+            UIView.animate(withDuration: 1.5) {
+                self.doneButton.alpha = 1.0
+            }
+        }
+    }
+
 }
